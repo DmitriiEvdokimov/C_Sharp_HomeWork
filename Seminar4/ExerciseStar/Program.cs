08f7f0fdd34965483fada9ad6391c1615fa3c4bb
Dmitrii Evdokimov <whiteliquid913@gmail.com>
2022-10-18 16:22:41 +0300

    exStar done

---
+++ Seminar4/ExerciseStar/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача “со звездочкой”: Напишите функцию,
+//  которая принимает одно число - высоту елочки и рисует ее в консоли звездочками.
+
+
+void Drawing(int top)
+{
+    Console.Clear();
+    int x = top;
+    int y = 0;
+    int count = 1;
+    while (y < top)
+    {
+        while (x < count+top)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine("*");
+            x++;
+        }
+        x = top;
+        x = x - count;
+        count = count + 1;
+        y++;
+    }
+
+}
+
+
+
+Console.Clear();
+Console.WriteLine("Нарисовать ёлочку.");
+Console.WriteLine("Введите высоту ёлочки:");
+int height = int.Parse(Console.ReadLine()!);
+Drawing(height);
