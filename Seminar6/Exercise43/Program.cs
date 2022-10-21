de9dd8e2ddd1b45ee48b984a6d72501cee6dbb5d
Dmitrii Evdokimov <whiteliquid913@gmail.com>
2022-10-21 18:48:54 +0300

    41,43 done.star beta

---
+++ Seminar6/Exercise43/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double[] Coordinates(double b1, double k1, double b2, double k2)
+{
+    double x = 0;
+    double y = 0;
+    x = (b2 - b1) / (k1 - k2);
+    y = k2 * x + b2;
+    double[] result = { x, y };
+    return result;
+}
+
+void PrintArray(double[] col)
+{
+    Console.Write("Координаты точки пересечения равны: (");
+    Console.Write(String.Join(" ,", col));
+    Console.Write(")");
+}
+
+Console.Clear();
+Console.WriteLine("Найти точку пересечения двух прямых по уравнению y = k * x + b");
+Console.WriteLine("Введите число b для первой прямой");
+Console.Write("b1=");
+double userB1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите угловой коэфициент k для первой прямой");
+Console.Write("k1=");
+double userK1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b для второй прямой");
+Console.Write("b2=");
+double userB2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите угловой коэфициент k для второй прямой");
+Console.Write("k2=");
+double userK2 = double.Parse(Console.ReadLine()!);
+
+if (userK1 == userK2 && userB1 == userB2) Console.WriteLine("Прямые совпадают");
+else if (userK1 == userK2) Console.WriteLine("Прямые параллельны.");
+else
+{
+    double[] result = Coordinates(userB1, userK1, userB2, userK2);
+    PrintArray(result);
+}
---
+++ Seminar6/ExerciseStar/Program.cs
@@ -0,0 +1,114 @@
+﻿// Задача со звездочкой: Напишите программу, 
+// которая реализует обход введенного двумерного массива, 
+// начиная с крайнего нижнего левого элемента против часовой стрелки.
+// 1 2 3
+// 4 5 6 -> 7 8 9 6 3 2 1 4 5
+// 7 8 9
+
+
+void PrintMatrix(int[,] mat)
+{
+    for (int i = 0; i < mat.GetLength(0); i++)
+    {
+        for (int j = 0; j < mat.GetLength(1); j++)
+        {
+            Console.Write($"{mat[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] CountClock(int[,] matrix)
+{
+    var n = matrix.GetLength(0);
+    var r = new int[n * n];
+    int j = -1, i = 0;
+    var h = true;
+    var d = !h;
+    int o = 0;
+    int p = n;
+    int m = n;
+    for (var c = 1; c <= matrix.Length; c++)
+    {
+        p--;
+        r[c - 1] = matrix[h ? i : !d ? ++i : --i, !h ? j : !d ? ++j : --j];
+        if (p <= 0 && matrix.Length != 2)
+        {
+            h = !h;
+            if ((o + 1) % 2 == 0) d = !d;
+            if (c == n || o > 1 && (o + 1) % 2 != 0) --m;
+            p = m;
+            o++;
+        }
+    }
+    return r;
+}
+
+
+int[,] userMatrix =
+{{1, 2, 3},
+{4, 5, 6},
+{7, 8, 9}};
+
+PrintMatrix(userMatrix);
+int[] result = CountClock(userMatrix);
+Console.WriteLine(String.Join(" ,",result));
+
+
+
+
+// int[] ReadMatrix(int row, int col)
+// {
+//     int[] result = new int[userMatrix.GetLength(0) * userMatrix.GetLength(1)];
+//     row = userMatrix.GetLength(0);
+//     col = 0;
+//     for (int i = 0; i < result.Length; i++)
+//     {
+//         result[i] = userMatrix[row, col];
+//         ReadMatrix(row-1, col);
+//         ReadMatrix(row - 1, col);
+//         ReadMatrix(row + 1, col);
+//         ReadMatrix(row, col + 1);
+//     }
+//     return result;
+// }
+
+
+// int[] ReadMatrix(int[,] mat)
+// {
+//     int[] result = new int[mat.GetLength(0) * mat.GetLength(1)];
+//     int j = 0;
+//     int x = mat.GetLength(0) - 1;
+//     int y = 0;
+//     int count = 0;
+//     for (int i = 0; i < mat.GetLength(1); i++)
+//     {
+//         result[j] = mat[x, y];
+//         y++;
+//         j++;
+//     }
+//     count++;
+//     for (int i = 0; i < mat.GetLength(0); i++)
+//     {
+//         result[j] = mat[x - count, y];
+//         x--;
+//         j++;
+//     }
+//     return result;
+// }
+
+
+// int[] CountClock(int[,] matrix)
+// {
+//     int x = 0;
+//     int y = matrix.GetLength(1) - 1
+
+//     int[] result = new int[matrix.GetLength(0) * matrix.GetLength(1)];
+//     for (int i = 0; i < matrix.GetLength(0) * matrix.GetLength(1); i++)
+//     {
+//         result[i] = matrix[, ];
+//         CountClock(matrix);
+//     }
+//     return result;
+// }
+
