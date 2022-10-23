4447df0f0d0144872bccd30dd24497a89bb19c9e
Dmitrii Evdokimov <whiteliquid913@gmail.com>
2022-10-24 00:51:12 +0300

    All done

--- Seminar6/ExerciseStar/Program.cs
+++ Seminar6/ExerciseStar/Program.cs
@@ -5,6 +5,17 @@
 // 4 5 6 -> 7 8 9 6 3 2 1 4 5
 // 7 8 9
 
+int[,] Fillmatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 100);
+        }
+    }
+    return matr;
+}
 
 void PrintMatrix(int[,] mat)
 {
@@ -18,97 +29,44 @@ void PrintMatrix(int[,] mat)
     }
 }
 
+
+
+
+
 int[] CountClock(int[,] matrix)
 {
-    var n = matrix.GetLength(0);
-    var r = new int[n * n];
+    var rows = matrix.GetLength(0);
+    var result = new int[rows * rows];
     int j = -1, i = 0;
     var h = true;
     var d = !h;
     int o = 0;
-    int p = n;
-    int m = n;
+    int p = rows;
+    int columns = rows;
     for (var c = 1; c <= matrix.Length; c++)
     {
         p--;
-        r[c - 1] = matrix[h ? i : !d ? ++i : --i, !h ? j : !d ? ++j : --j];
+        result[c - 1] = matrix[h ? i : !d ? ++i : --i, !h ? j : !d ? ++j : --j];
         if (p <= 0 && matrix.Length != 2)
         {
             h = !h;
             if ((o + 1) % 2 == 0) d = !d;
-            if (c == n || o > 1 && (o + 1) % 2 != 0) --m;
-            p = m;
+            if (c == rows || o > 1 && (o + 1) % 2 != 0) --columns;
+            p = columns;
             o++;
         }
     }
-    return r;
+    return result;
 }
 
-
-int[,] userMatrix =
-{{1, 2, 3},
-{4, 5, 6},
-{7, 8, 9}};
-
-PrintMatrix(userMatrix);
-int[] result = CountClock(userMatrix);
-Console.WriteLine(String.Join(" ,",result));
-
-
-
-
-// int[] ReadMatrix(int row, int col)
-// {
-//     int[] result = new int[userMatrix.GetLength(0) * userMatrix.GetLength(1)];
-//     row = userMatrix.GetLength(0);
-//     col = 0;
-//     for (int i = 0; i < result.Length; i++)
-//     {
-//         result[i] = userMatrix[row, col];
-//         ReadMatrix(row-1, col);
-//         ReadMatrix(row - 1, col);
-//         ReadMatrix(row + 1, col);
-//         ReadMatrix(row, col + 1);
-//     }
-//     return result;
-// }
-
-
-// int[] ReadMatrix(int[,] mat)
-// {
-//     int[] result = new int[mat.GetLength(0) * mat.GetLength(1)];
-//     int j = 0;
-//     int x = mat.GetLength(0) - 1;
-//     int y = 0;
-//     int count = 0;
-//     for (int i = 0; i < mat.GetLength(1); i++)
-//     {
-//         result[j] = mat[x, y];
-//         y++;
-//         j++;
-//     }
-//     count++;
-//     for (int i = 0; i < mat.GetLength(0); i++)
-//     {
-//         result[j] = mat[x - count, y];
-//         x--;
-//         j++;
-//     }
-//     return result;
-// }
-
-
-// int[] CountClock(int[,] matrix)
-// {
-//     int x = 0;
-//     int y = matrix.GetLength(1) - 1
-
-//     int[] result = new int[matrix.GetLength(0) * matrix.GetLength(1)];
-//     for (int i = 0; i < matrix.GetLength(0) * matrix.GetLength(1); i++)
-//     {
-//         result[i] = matrix[, ];
-//         CountClock(matrix);
-//     }
-//     return result;
-// }
-
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int b = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[a, b];
+matrix = Fillmatrix(matrix);
+PrintMatrix(matrix);
+int[] array = CountClock(matrix);
+Console.WriteLine("______________________________________");
+Console.WriteLine(String.Join(" ,",array));
---
+++ Seminar6/ExerciseStarMyself/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача со звездочкой: Напишите программу, 
+// которая реализует обход введенного двумерного массива, 
+// начиная с крайнего нижнего левого элемента против часовой стрелки.
+// 1 2 3
+// 4 5 6 -> 7 8 9 6 3 2 1 4 5
+// 7 8 9
+
+
+int[,] GetBinaryArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+
+void PrintBinaryArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[] CounterClockArray(int[,] matrix)
+{
+    int count = 0;
+    int x = matrix.GetLength(0) - 1;
+    int y = 0;
+    int n = 0;
+    int[] finalArray = new int[matrix.Length];
+    for (int i = 0; i < matrix.Length; i++)
+
+    {
+        while (y < matrix.GetLength(1) - count && n < matrix.Length)
+        {
+            finalArray[n++] = matrix[x, y];
+            y++;
+        }
+        x--;
+        y--;
+        while (x > 0 + count && n < matrix.Length)
+        {
+            finalArray[n++] = matrix[x, y];
+            x--;
+        }
+        while (y >= 0 + count && n < matrix.Length)
+        {
+            finalArray[n++] = matrix[x, y];
+            y--;
+        }
+        x++;
+        y++;
+        count++;
+        while (x < matrix.GetLength(0) - count && n < matrix.Length)
+        {
+            finalArray[n++] = matrix[x, y];
+            x++;
+        }
+        x--;
+        y++;
+    }
+    return finalArray;
+}
+
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве:");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве:");
+int col = int.Parse(Console.ReadLine()!);
+
+int[,] userArray = GetBinaryArray(row, col, 1, 100);
+PrintBinaryArray(userArray);
+Console.WriteLine("____________________");
+int[] endArray = CounterClockArray(userArray);
+Console.WriteLine(String.Join(" ,", endArray));
