0ce2ec8825268d1c580438bb115192d1121f59e7
Dmitrii Evdokimov <whiteliquid913@gmail.com>
2022-11-18 01:40:56 +0300

    all done

---
+++ Seminar9/ExerciseStar/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача со звездочкой: Пользователь вводит скобочные последовательности. В последовательности могут встретиться скобки вида: {}, (), [].
+// Правильная скобочная последовательность - символьная последовательность, составленная в алфавите, состоящем из символов, сгруппированных в упорядоченные пары.
+// Пример правильной скобочной последовательности: (), ()[]{}
+// Пример неправильной: (], ({)}
+// Написать программу, которая определяет правильная ли скобочная последовательность была введена
+
+
+
+bool OrderOfBracketCheck(string userText)
+{
+    string stack = string.Empty;
+    int top = -1;
+    for (int i = 0; i < userText.Length; i++)
+    {
+
+        if (userText[i] == '(')
+        {
+            stack += '(';
+            top++;
+        }
+        else if (userText[i] == '{')
+        {
+            stack += '{';
+            top++;
+        }
+        else if (userText[i] == '[')
+        {
+            stack += '[';
+            top++;
+        }
+
+        else if (userText[i] == ')' && stack == string.Empty) return false;
+        else if (userText[i] == '}' && stack == string.Empty) return false;
+        else if (userText[i] == ']' && stack == string.Empty) return false;
+
+        else if (userText[i] == ')')
+        {
+            if (stack[top] == '(') stack = stack.Remove(top--);
+            else return false;
+        }
+        else if (userText[i] == '}')
+        {
+            if (stack[top] == '{') stack = stack.Remove(top--);
+            else return false;
+        }
+        else if (userText[i] == ']')
+        {
+            if (stack[top] == '[') stack = stack.Remove(top--);
+            else return false;
+        }
+    }
+    if (stack == string.Empty) return true;
+    else return false;
+}
+
+Console.WriteLine("Введите последовательность скобок");
+string text = Console.ReadLine();
+Console.WriteLine();
+if (OrderOfBracketCheck(text)) Console.WriteLine("Последователность верна");
+else Console.WriteLine("Последовательность не верна");
+Console.WriteLine();
+
+
---
+++ Seminar9/ExerciseStarRec/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача со звездочкой: Пользователь вводит скобочные последовательности. В последовательности могут встретиться скобки вида: {}, (), [].
+// Правильная скобочная последовательность - символьная последовательность, составленная в алфавите, состоящем из символов, сгруппированных в упорядоченные пары.
+// Пример правильной скобочной последовательности: (), ()[]{}
+// Пример неправильной: (], ({)}
+// Написать программу, которая определяет правильная ли скобочная последовательность была введена
+
+string StringBuilder(string text, int index, char replacement = '*')
+{
+    text = text.Remove(index, 1).Insert(index, replacement.ToString());
+    return text;
+}
+
+
+bool OrderOfBracketCheckRec(string userText)
+{
+    for (int i = 0; i < userText.Length; i++)
+    {
+        if (userText[i] == ')')
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+
+                if (userText[j] == '(')
+                {
+                    userText = StringBuilder(userText, i);
+                    userText = StringBuilder(userText, j);
+                    OrderOfBracketCheckRec(userText);
+                    break;
+                }
+                if (userText[j] == '{') return false;
+                if (userText[j] == '[') return false;
+            }
+        }
+        if (userText[i] == '}')
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (userText[j] == '{')
+                {
+                    userText = StringBuilder(userText, i);
+                    userText = StringBuilder(userText, j);
+                    OrderOfBracketCheckRec(userText);
+                    break;
+                }
+                if (userText[j] == '(') return false;
+                if (userText[j] == '[') return false;
+            }
+        }
+        if (userText[i] == ']')
+        {
+            for (int j = i - 1; j >= 0; j--)
+            {
+                if (userText[j] == '[')
+                {
+                    userText = StringBuilder(userText, i);
+                    userText = StringBuilder(userText, j);
+                    OrderOfBracketCheckRec(userText);
+                    break;
+                }
+                if (userText[j] == '(') return false;
+                if (userText[j] == '{') return false;
+            }
+        }
+    }
+    foreach (var item in userText)
+    {
+        if (item == '(' || item == ')'
+          || item == '{'|| item == '}'
+          || item == '['|| item == ']') return false;
+    }
+    return true;
+}
+
+
+Console.WriteLine("Введите последовательность скобок");
+string text = Console.ReadLine();
+Console.WriteLine();
+if (OrderOfBracketCheckRec(text)) Console.WriteLine("Последователность верна");
+else Console.WriteLine("Последовательность не верна");
+Console.WriteLine();
+
